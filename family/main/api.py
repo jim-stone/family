1f9713d9f4ab0ab59a49c430900cc33332ea2eb2
jim-stone <jakub.gustaw.kaminski@gmail.com>
2024-04-14 18:44:28 +0200

    family tests found error

--- family/main/api.py
+++ family/main/api.py
@@ -80,6 +80,7 @@ class FamilyViewset(viewsets.ModelViewSet):
         serializer = FamilySerializer(data=request.data,
                                       context={'request': request})
         if serializer.is_valid():
+            serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
--- family/main/tests/test_api.py
+++ family/main/tests/test_api.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import User
 from rest_framework.test import APIRequestFactory, APITestCase
-from main import serializers, models, api
+from main import models, api
 
 
 class TestCreateFamily(APITestCase):
@@ -12,13 +12,25 @@ class TestCreateFamily(APITestCase):
                                              password='kuba')
         self.data = dict(name='Cała kolonia')
 
-    def test_can_create_family_with_proper_parameters(self):
+    def test_can_create_family_with_founder_member_name(self):
+        request = self.factory.post('/families/', self.data)
+        request.user = self.user
+        self.view(request)
+        family = models.Family.objects.get(**self.data)
+        self.assertEquals(family.members.first().name, self.user.username)
 
+    def test_founder_member_is_family_boss(self):
+        request = self.factory.post('/families/', self.data)
+        request.user = self.user
+        self.view(request)
+        member = models.Member.objects.first()
+        self.assertTrue(member.is_boss)
+
+    def test_correct_response_if_ok(self):
         request = self.factory.post('/families/', self.data)
         request.user = self.user
         response = self.view(request)
-        print(response.data, response.status_code)
-        self.assertEqual(response.status_code, 201)
+        self.assertEquals(response.status_code, 201)
 
     def tearDown(self) -> None:
         return super().tearDown()
